76abef07eb634040a8367b338640ba5509edee85
nip403 <41174986+nip403@users.noreply.github.com>
2018-11-11 19:09:40 +0000

    Add files via upload

---
+++ engine.py
@@ -0,0 +1,164 @@
+import pygame
+import random
+import time
+import numpy
+
+class Square:
+    def __init__(self,state=0):
+        self.state = state
+        self.number = 1
+        
+        self.covered = True
+        self.flagged = False
+
+    def revealAll(self,grid,ref):
+        if self.flagged:
+            return
+
+        self.covered = False
+        neighbours = [i for i in [[ref[0]-1,ref[1]-1],[ref[0],ref[1]-1],[ref[0]+1,ref[1]-1],[ref[0]-1,ref[1]],[ref[0]+1,ref[1]],[ref[0]-1,ref[1]+1],[ref[0],ref[1]+1],[ref[0]+1,ref[1]+1]] if (i[0] >= 0 and i[1] >= 0 and i[0] < len(grid[0]) and i[1] < len(grid))]
+
+        for n in neighbours:
+            if not grid[n[1]][n[0]].number and grid[n[1]][n[0]].covered:
+                grid[n[1]][n[0]].revealAll(grid,n)
+            else:
+                grid[n[1]][n[0]].covered = False
+
+class Grid:
+    colours = {1:(0,0,255),2:(50,205,50),3:(255,140,0),4:(255,0,0),5:(148,0,211),6:(220,20,60),7:(0,206,209),8:(255,105,180)}
+    mine_distribution = 8
+    
+    def __init__(self,surf_size,square_size,blit_dest=[0,0]):
+        self.surf = pygame.Surface(surf_size)
+        self.resolution = [int(i/square_size) for i in self.surf.get_size()]
+        
+        self.size = square_size
+        self.blit_dest = blit_dest
+        
+        self.isGenerated = False
+        self.minesCount = int(numpy.prod(self.resolution)//self.mine_distribution)
+        self.flags = self.minesCount
+
+        self.grid = [[Square() for _ in range(self.resolution[0])] for _ in range(self.resolution[1])]
+        self.font = pygame.font.SysFont("Garamond MS",self.size)
+        
+    def generate(self,pos):
+        count = 0
+
+        while count < self.minesCount:
+            for p,i in enumerate(self.grid):
+                for q,j in enumerate(i):
+                    if random.randint(1,self.mine_distribution) == 1 and not [q,p] in [i for i in [pos,[pos[0]-1,pos[1]-1],[pos[0],pos[1]-1],[pos[0]+1,pos[1]-1],[pos[0]-1,pos[1]],[pos[0]+1,pos[1]],[pos[0]-1,pos[1]+1],[pos[0],pos[1]+1],[pos[0]+1,pos[1]+1]] if (i[0] >= 0 and i[1] >= 0 and i[0] < len(self.grid[0]) and i[1] < len(self.grid))] and not j.number == -1 and count < self.minesCount:
+                        j.number = -1
+                        count += 1
+
+        for y,row in enumerate(self.grid):
+            for x,sq in enumerate(row):
+                surroundingMines = 0
+
+                if sq.number == -1:
+                    continue
+
+                combinations = [[x-1,y-1],[x,y-1],[x+1,y-1],[x-1,y],[x+1,y],[x-1,y+1],[x,y+1],[x+1,y+1]]
+                for combination in combinations:
+                    if combination[0] >= 0 and combination[0] < len(self.grid[0]) and combination[1] >= 0 and combination[1] < len(self.grid):
+                        if self.grid[combination[1]][combination[0]].number == -1:
+                            surroundingMines += 1
+                            
+                sq.number = surroundingMines
+
+        self.isGenerated = True
+
+    def move(self,pos,button):
+        gridRef = self.coords_to_gridRef(pos)
+        
+        if gridRef is None:
+            return
+        if not self.isGenerated:
+            self.generate(gridRef)
+
+        if button == 1 and not self.grid[gridRef[1]][gridRef[0]].flagged:
+            if self.grid[gridRef[1]][gridRef[0]].number:
+                self.grid[gridRef[1]][gridRef[0]].covered = False
+            else:
+                self.grid[gridRef[1]][gridRef[0]].revealAll(self.grid,gridRef)
+        elif button == 3:
+            if self.flags > 0 and not self.grid[gridRef[1]][gridRef[0]].flagged:
+                self.grid[gridRef[1]][gridRef[0]].flagged = True
+                self.flags -= 1
+            elif self.grid[gridRef[1]][gridRef[0]].flagged:
+                self.grid[gridRef[1]][gridRef[0]].flagged = False
+                self.flags += 1
+        
+    def coords_to_gridRef(self,pos):
+        return [int((pos[i]-self.blit_dest[i])//self.size) for i in range(2)] if all(x < self.resolution[p] for p,x in enumerate([int((pos[i]-self.blit_dest[i])//self.size) for i in range(2)])) else None
+
+    def draw(self,surf):
+        for y,row in enumerate(self.grid):
+            for x,square in enumerate(row):
+                if not square.covered:
+                    pygame.draw.rect(self.surf,(255,255,255),(x*self.size,y*self.size,self.size,self.size),0)
+
+                    if square.number == -1:
+                        pygame.draw.circle(self.surf,(255,0,0),[int(x*self.size+(self.size/2)),int(y*self.size+(self.size/2))],int(self.size/4),0)
+                    elif square.number:
+                        msg = self.font.render(str(square.number),True,self.colours[square.number])
+                        self.surf.blit(msg,msg.get_rect(center=[x*self.size+(self.size/2),y*self.size+(self.size/2)]))
+                else:
+                    pygame.draw.rect(self.surf,(150,150,150),(x*self.size,y*self.size,self.size,self.size),0)
+
+                    if square.flagged:
+                        pygame.draw.polygon(self.surf,(255,0,0),[[x*self.size+(0.3*self.size),y*self.size+(0.2*self.size)],[x*self.size+(0.4*self.size),y*self.size+(0.2*self.size)],[x*self.size+(0.4*self.size),y*self.size+(0.22*self.size)],[x*self.size+(0.8*self.size),y*self.size+(0.4*self.size)],[x*self.size+(0.4*self.size),y*self.size+(0.6*self.size)],[x*self.size+(0.4*self.size),y*self.size+(0.8*self.size)],[x*self.size+(0.3*self.size),y*self.size+(0.8*self.size)]],0)
+
+        for x in range(1,len(self.grid[0])):
+            pygame.draw.line(self.surf,(0,0,0),[x*self.size,0],[x*self.size,surf.get_height()],1)
+        for y in range(1,len(self.grid)):
+            pygame.draw.line(self.surf,(0,0,0),[0,y*self.size],[surf.get_width(),y*self.size],1)
+
+        surf.blit(self.surf,self.blit_dest)
+
+    def check_game_status(self):
+        return True if ((not any(any((s.flagged and not s.number == -1) or (not s.flagged and s.number == -1) for s in row_) for row_ in self.grid)) and all(all(not t.covered or (t.covered and t.flagged) for t in _row) for _row in self.grid)) else (False if any(any(g.number == -1 and not g.covered for g in row) for row in self.grid) else None) 
+
+    def reset(self,square_size=None):
+        if square_size is not None:
+            self.resolution = [int(i/square_size) for i in self.surf.get_size()]
+            self.size = square_size
+            self.minesCount = int(numpy.prod(self.resolution)//self.mine_distribution)
+            self.font = pygame.font.SysFont("Garamond MS",self.size)
+        
+        self.grid = [[Square() for _ in range(self.resolution[0])] for _ in range(self.resolution[1])]
+        self.isGenerated = False
+        self.flags = self.minesCount
+
+class Banner:
+    def __init__(self,surf_size,grid,blit_dest=[0,0]):
+        self.grid = grid
+        self.surf = pygame.Surface(surf_size)
+
+        self.t0 = time.time()
+        self.blit_dest = blit_dest
+        self.h1 = pygame.font.SysFont("Garamond MS",50)
+        self.h2 = pygame.font.SysFont("Garamond MS",30)
+
+    def update(self):
+        self.surf.fill((250,250,255))
+        pygame.draw.line(self.surf,(0,0,0),[0,0],[0,self.surf.get_height()],4)
+        
+        head = self.h1.render("Minesweeper",False,(0,0,0))
+        foot = self.h2.render("by NIP",False,(0,0,0))
+
+        self.surf.blit(head,head.get_rect(center=[self.surf.get_width()/2,self.surf.get_height()/12]))
+        self.surf.blit(foot,foot.get_rect(center=[self.surf.get_width()/2,self.surf.get_height()/7]))
+
+        flg = self.h2.render(f"Flags Remaining: {self.grid.flags}/{self.grid.minesCount}",True,(0,0,0))
+        self.surf.blit(flg,flg.get_rect(center=[self.surf.get_width()/2,self.surf.get_height()/2.5]))
+
+        tim = self.h2.render(f"Time Elapsed: {int(time.time()-self.t0)}",True,(0,0,0))
+        self.surf.blit(tim,tim.get_rect(center=[self.surf.get_width()/2,self.surf.get_height()/2]))
+
+    def draw(self,surf):
+        surf.blit(self.surf,self.blit_dest)
+
+    def reset(self):
+        self.t0 = time.time()
---
+++ main.py
@@ -0,0 +1,143 @@
+import engine
+import pygame
+import sys
+
+# add custom resolution menu
+
+s = [1000,700]
+board_size = [700,s[1]]
+banner_size = [s[0]-board_size[0],s[1]]
+
+pygame.init()
+screen = pygame.display.set_mode(s,0,32)
+pygame.display.set_caption("Minesweeper by NIP")
+
+h1 = pygame.font.SysFont("Garamond MS",40)
+h2 = pygame.font.SysFont("Garamond MS",20)
+
+def menu():
+    standard = (100,255,100)
+    onHover = (250,250,20)
+
+    head = pygame.font.SysFont("Garamond MS",65).render("Minesweeper",True,(0,0,0))
+    version = h2.render("version 1.0.0",True,(0,0,0))
+    foot = h2.render("by NIP",True,(0,0,0))
+    
+    button1 = pygame.Rect(screen.get_width()/2 - 200,screen.get_height()/5,400,screen.get_height() * (3/20))
+    button2 = pygame.Rect(screen.get_width()/2 - 200,screen.get_height() * (2/5),400,screen.get_height() * (3/20))
+    button3 = pygame.Rect(screen.get_width()/2 - 200,screen.get_height() * (3/5),400,screen.get_height() * (3/20))
+    button4 = pygame.Rect(screen.get_width()/2 - 200,screen.get_height() * (4/5),400,screen.get_height() * (3/20))
+
+    b1 = h1.render("Easy",True,(40,150,20))
+    b2 = h1.render("Medium",True,(255,128,0))
+    b3 = h1.render("Hard",True,(255,0,0))
+    b4 = h1.render("Impossible",True,(0,0,0))
+    
+    while True:
+        mouse = pygame.Rect([i-1 for i in pygame.mouse.get_pos()],[2,2])
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if mouse.colliderect(button1):
+                    return 100
+                elif mouse.colliderect(button2):
+                    return 50
+                elif mouse.colliderect(button3):
+                    return 25
+                elif mouse.colliderect(button4):
+                    return 10
+        
+        screen.fill((255,255,255))
+        pygame.draw.rect(screen,standard if not mouse.colliderect(button1) else onHover,button1,0)
+        pygame.draw.rect(screen,standard if not mouse.colliderect(button2) else onHover,button2,0)
+        pygame.draw.rect(screen,standard if not mouse.colliderect(button3) else onHover,button3,0)
+        pygame.draw.rect(screen,standard if not mouse.colliderect(button4) else onHover,button4,0)
+
+        screen.blit(head,head.get_rect(center=[screen.get_width()/2,screen.get_height()/10]))
+        screen.blit(version,version.get_rect(center=[screen.get_width()/2,screen.get_height()/7]))
+        screen.blit(foot,foot.get_rect(center=[screen.get_width()/2,screen.get_height()/6]))
+
+        screen.blit(b1,b1.get_rect(center=button1.center))
+        screen.blit(b2,b2.get_rect(center=button2.center))
+        screen.blit(b3,b3.get_rect(center=button3.center))
+        screen.blit(b4,b4.get_rect(center=button4.center))
+
+        pygame.display.flip()
+
+def result(grid,banner):
+    cover = pygame.Surface(screen.get_size())
+    cover.set_alpha(200)
+    cover.fill((0,0,0))
+
+    msg = h1.render(f"You {'Won' if grid.check_game_status() else 'Lost'}",True,(255,255,255))
+    end = h2.render("Press any key to continue",True,(255,255,255))
+
+    cover.blit(msg,msg.get_rect(center=[cover.get_width()/2,cover.get_height()/2.5]))
+    cover.blit(end,end.get_rect(center=[cover.get_width()/2,cover.get_height()/2]))
+
+    x,y,cycle,wave = 0,0,True,[[0,0]]
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                return
+
+        grid.draw(screen)
+        banner.draw(screen)
+        screen.blit(cover,(0,0))
+
+        pygame.display.flip()
+
+        if cycle:
+            for i in wave:
+                try:
+                    grid.grid[i[1]][i[0]].covered = False
+                except:
+                    pass
+
+            new = []
+            for n in [e for e in [[i[0]+1,i[1]] for i in wave]+[[i[0],i[1]+1] for i in wave] if e[0] < len(grid.grid[0]) and e[1] < len(grid.grid)]:
+                if not n in new:
+                    new.append(n)
+            wave = new[:]
+
+            if not len(wave):
+                cycle = False
+                    
+def main():
+    square_size = menu()
+    grid = engine.Grid(board_size,square_size)
+    banner = engine.Banner(banner_size,grid,[grid.surf.get_width(),0])
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                grid.move(pygame.mouse.get_pos(),event.button)
+
+        banner.update()
+
+        grid.draw(screen)
+        banner.draw(screen)
+
+        pygame.display.flip()
+
+        if grid.check_game_status() is not None:
+            result(grid,banner)
+            square_size = menu()
+            grid.reset(square_size)
+            banner.reset()
+        
+if __name__ == "__main__":
+    main()
